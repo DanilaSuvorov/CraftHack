55f0187e28c17393cd15c64b5f8f4a22ff12442a
Fedorova Margarita <margarita160316@yandex.ru>
2019-08-09 16:13:01 +0300

    Splash Activity

--- app/src/main/java/com/example/crafthack/SplashActivity.java
+++ app/src/main/java/com/example/crafthack/SplashActivity.java
@@ -1,4 +1,30 @@
 package com.example.crafthack;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.radiation);
+        Button start = findViewById(R.id.button2);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this, LocationComponentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
